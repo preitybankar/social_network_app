4bab6787b37a4b3fa5b10044fcb2b25e95612e74
priti <preity.bankar@gmail.com>
2012-09-14 19:10:44 +0530

    Initial Commit

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,15 @@
+class HomesController < ApplicationController
+  def index
+    @user = User.new
+  end
+  
+  def create  
+    @user = User.new(params[:user])  
+    if @user.save  
+      redirect_to root_url, :notice => "You Have Registered successfully !"  
+    else  
+      render "new"  
+    end  
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialNetworkApp::Application.config.secret_token = 'ca3f8e2a405c9d664335e70be2881772b54f73a4200db4728c3d586e12d4f170554ffac72bd7f673f992e081e72dc1258a1c08c273b853bcecf36f8cf5023668'
